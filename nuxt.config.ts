368d9972dd6832b824e5f747d43a6f58b34c0508
Lukas Trumm <lukas.trumm@gmail.com>
2025-06-17 09:22:20 +0200

    Nuxt project initialized with complete setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,45 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/plausible",
+    "@nuxtjs/seo",
+    "@vueuse/nuxt",
+  ],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   devtools: { enabled: true },
+  site: {
+    url: "https://example.com",
+    name: "Website name",
+    description: "Website description",
+    defaultLocale: "en",
+    indexable: false,
+  },
   future: {
-    compatibilityVersion: 4
-  }
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    typedPages: true,
+  },
+  compatibilityDate: "2025-04-01",
+  eslint: {
+    config: {
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+  plausible: {
+    ignoredHostnames: ["localhost"],
+    apiHost: "https://plausible.lttr.cz",
+  },
+  // Custom styles
+  css: ["~/assets/css/main.css"],
 })
