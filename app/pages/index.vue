c60e5a2d58a57fe904b77fcb4785d1fda13560e6
Lukas Trumm <lukas.trumm@gmail.com>
2025-06-19 14:41:11 +0200

    Base index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>This is glossary</div>
+  <UPage>
+    <UPageHero
+      title="Welcome to Glosssssary"
+      description="A glossary full of wierd technical terms"
+      :ui="{
+        title: 'font-display',
+        description: 'font-display',
+      }"
+    />
+    <UPageGrid>
+      <UPageCard v-for="(card, index) of cards" :key="index" v-bind="card" />
+    </UPageGrid>
+  </UPage>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+definePageMeta({
+  layout: "default",
+})
+
+const cards = [
+  {
+    title: "AI",
+    icon: "uil:robot",
+    to: "/ai",
+  },
+  {
+    title: "Web performance",
+    icon: "uil:monitor-heart-rate",
+    to: "/web-performance",
+  },
+]
+</script>
 
 <style scoped></style>
