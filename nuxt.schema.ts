8abc111a00145b2c702ff2ffc6b3263fd776f398
Lukas Trumm <lukas.trumm@gmail.com>
2025-07-16 13:01:38 +0200

    Align nuxt.schema with app.config

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -2,18 +2,33 @@ import { field, group } from "@nuxt/content/preview"
 
 export default defineNuxtSchema({
   appConfig: {
-    parent: group({
+    ui: group({
       title: "UI",
       description: "UI configuration",
       icon: "i-ph-palette-fill",
       fields: {
-        primary: field({
-          type: "string",
-          title: "Primary",
-          description: "Primary color of your UI.",
+        colors: group({
+          title: "Colors",
+          description: "Colors of your UI.",
           icon: "i-ph-palette",
-          default: "mandarin",
-          required: ["mint", "rose", "amber"],
+          fields: {
+            primary: field({
+              type: "string",
+              title: "Primary",
+              description: "Primary color of your UI.",
+              icon: "i-ph-palette",
+              default: "mandarin",
+              required: ["mandarin", "mint", "rose", "amber"],
+            }),
+            neutral: field({
+              type: "string",
+              title: "Neutral",
+              description: "Neutral color of your UI.",
+              icon: "i-ph-palette",
+              default: "stone",
+              required: ["stone", "slate", "gray"],
+            }),
+          },
         }),
       },
     }),
