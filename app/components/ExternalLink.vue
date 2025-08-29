a0dabc42f7d7f1cdbd3ed50a109c764b7b12bfee
Lukas Trumm <lukas.trumm@gmail.com>
2025-08-29 14:41:06 +0200

    Add external link component

---
+++ app/components/ExternalLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLink :to external>
+    <span class="hover:underline">{{ text }}</span>
+    <Icon
+      name="uil:external-link-alt"
+      class="w-5 h-5 ms-1 relative top-[2px]"
+    />
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from "#components"
+
+defineProps<{
+  to: string
+  text: string
+}>()
+</script>
+
+<style scoped></style>
